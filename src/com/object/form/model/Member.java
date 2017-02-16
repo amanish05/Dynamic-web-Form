5e004f6d107a517966ee1b36682f8bee09985a3f
Yuan Chang <cy@Yuans-MacBook-Air.local>
2017-02-16 11:23:51 -0800

    modifyied Member class properties

--- src/com/object/form/model/Member.java
+++ src/com/object/form/model/Member.java
@@ -8,7 +8,9 @@ public class Member {
 	private String firstName;
 	private String lastName;
 	private String middleName;
+	private String gender;
 	private String email;
+	private String userName;
 	private Address address;
 	private char[] passcode;
 	private List<Role> roles;
@@ -37,11 +39,23 @@ public class Member {
 	public void setMiddleName(String middleName) {
 		this.middleName = middleName;
 	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 	public Address getAddress() {
 		return address;
